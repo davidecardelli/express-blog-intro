a285de4c7abd5d8e42e4b7103f9a2c77f6e93666
davidecardelli <davide.cardelli.94@gmail.com>
2023-11-13 14:15:39 +0100

    first commit

---
+++ db/posts.js
@@ -0,0 +1,32 @@
+[
+    {
+        titolo: "Viaggio estivo",
+        contenuto: "Questa estate ho viaggiato in luoghi incredibili, godendo del sole e della natura.",
+        immagine: "url_dell_immagine_1.jpg",
+        tags: ["viaggio", "estate", "natura"]
+    },
+    {
+        titolo: "Ricetta del giorno",
+        contenuto: "Oggi ho preparato una deliziosa ricetta con ingredienti freschi e gustosi.",
+        immagine: "url_dell_immagine_2.jpg",
+        tags: ["cucina", "ricetta", "gusto"]
+    },
+    {
+        titolo: "Nuovo libro da leggere",
+        contenuto: "Ho appena iniziato a leggere un libro emozionante che non riesco a mettere giù.",
+        immagine: "url_dell_immagine_3.jpg",
+        tags: ["libro", "lettura", "emozione"]
+    },
+    {
+        titolo: "Allenamento quotidiano",
+        contenuto: "Mantenere uno stile di vita attivo con un allenamento quotidiano è fondamentale per la salute.",
+        immagine: "url_dell_immagine_4.jpg",
+        tags: ["fitness", "salute", "allenamento"]
+    },
+    {
+        titolo: "Ultima avventura in montagna",
+        contenuto: "Escursione epica in montagna con paesaggi mozzafiato e avventure indimenticabili.",
+        immagine: "url_dell_immagine_5.jpg",
+        tags: ["avventura", "montagna", "escursione"]
+    }
+]
